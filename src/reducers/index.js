5d8501d3fb7d383dfd08ce0667fa0de790f33474
prosaicpoet123 <prosaicpoet@hotmail.co.uk>
2018-10-07 10:22:06 +0100

    initialises dummy data in redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { initialState } from '../selectors/selectors'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  shop: combineReducers({
+    items: (state = initialState.shop) => state,
+    basket: (state = initialState.basket) => state
+  })
 });
 
 export default rootReducer;
---
+++ src/selectors/selectors.js
@@ -0,0 +1,53 @@
+export const initialState = {
+	shop: {
+		items: [
+		{
+			id: "1",
+			name: "Product 1",
+			variants: [
+				{
+					name: "",
+				},
+			],
+		},
+		{
+			id: "2",
+			name: "Product 2",
+			variants: [
+				{
+					name: "",
+				},
+			],
+		},
+		{
+			id: "3",
+			name: "Product 3",
+			variants: [
+				{
+					name: "",
+				},
+			],
+		},
+		{
+			id: "4",
+			name: "Product 4",
+			variants: [
+				{
+					name: "",
+				},
+			],
+		},
+		{
+			id: "5",
+			name: "Product 5",
+			variants: [
+				{
+					name: "",
+				},
+			],
+		},
+	]},
+
+	basket: []
+
+}
